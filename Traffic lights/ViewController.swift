867e80ad22dec4ca806e95f1b58de6e8d8511e8e
imoiseeva <moiseeva.irina@gmail.com>
2021-03-09 20:44:16 +0200

    Сhanged constraints

--- Traffic lights/ViewController.swift
+++ Traffic lights/ViewController.swift
@@ -7,54 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum LightNow {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    
     @IBOutlet weak var redColour: UIView!
     @IBOutlet weak var yelloColour: UIView!
     @IBOutlet weak var greenColour: UIView!
     
     @IBOutlet weak var showText: UIButton!
+    private var lightNow = LightNow.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        showText.layer.cornerRadius = 10
         
         redColour.alpha = 0.3
         yelloColour.alpha = 0.3
         greenColour.alpha = 0.3
         
-        redColour.layer.cornerRadius = 60
-        yelloColour.layer.cornerRadius = 60
-        greenColour.layer.cornerRadius = 60
-        
-       // chengedColour()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redColour.layer.cornerRadius = redColour.frame.width / 2
+        yelloColour.layer.cornerRadius = yelloColour.frame.width / 2
+        greenColour.layer.cornerRadius = greenColour.frame.width / 2
         
     }
-
+    
+    
     @IBAction func startButtom(_ sender: Any) {
         
         redColour.alpha = 1
         showText.setTitle("NEXT", for: .normal)
         
-        chengedColour()
-    }
-
-    func chengedColour(){
-
-    
-    
-                if redColour.alpha == 1 {
-                    redColour.alpha = 0.3
-                    yelloColour.alpha = 1
-
-                } else if yelloColour.alpha == 1{
-                    redColour.alpha = 0.3
-                    yelloColour.alpha = 0.3
-                    greenColour.alpha = 1
-                } else if greenColour.alpha == 1 {
-                    redColour.alpha = 1
-                    yelloColour.alpha = 0.3
-                    greenColour.alpha = 0.3
-                }
+        switch lightNow {
+        case .red:
+            greenColour.alpha = 0.3
+            redColour.alpha = 1
+            lightNow = .yellow
+        case .yellow:
+            redColour.alpha = 0.3
+            yelloColour.alpha = 1
+            lightNow = .green
+        case .green:
+            lightNow = .red
+            yelloColour.alpha = 0.3
+            greenColour.alpha = 1
+        }
+        
+        
     }
 }
 
